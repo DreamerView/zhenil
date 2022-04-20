296bee99dcfb6f8d732e51269c7071924769d088
Dreamer <temirkhan.onyx@gmail.com>
2022-04-20 18:32:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+  <header>
+    <div className="header__logo">
+      <div className="header__logo_pic">
+        <img loading="lazy" className="header__logo_img" src="/img/logo.png" alt="Logo" />
+      </div>
     </div>
+    <div className="header__action">
+      <img loading="lazy" className="header__action_avatar" src="/img/3600ABB7-7824-467A-BB26-6E86CDD1EC91.png" alt="" />
+      <div className="header__action_block">
+        <span className="header__action_block_text">EN</span>
+        <div className="header__search_menu_pic">
+          <img className="header__search_menu_img" src="/img/top.svg" alt="icon" />
+        </div>
+      </div>
+    </div>
+    <div className="header__search">
+      <div className="header__search_menu">
+        <div className="header__search_menu_pic">
+          <img loading="lazy" className="header__search_menu_img" src="/img/menu.svg" alt="icon" />
+        </div>
+        <span className="header__search_menu_text">Menu</span>
+        <div className="header__search_menu_pic" id="search_top">
+          <img loading="lazy" className="header__search_menu_img" src="/img/top.svg" alt="icon" />
+        </div>
+      </div>
+      <input placeholder="Search over 30 million book titles" className="header__search_input" type="text" />
+      <button className="header__search_button">
+        <img loading="lazy" src="/img/search.svg" alt="icon" />
+      </button>
+    </div>
+  </header>
+  <main>
+    <div className="main__nav">
+      <span><b className="b-color">Home /</b> Books</span>
+    </div>
+  </main>
+</div>
+
   );
 }
 
