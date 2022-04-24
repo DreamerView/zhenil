03e95216431f84df927d6498ec4fbbb041ae9b27
Dreamer <temirkhan.onyx@gmail.com>
2022-04-24 18:02:20 +0600

    select menu added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <div>
+    <>
   <header>
     <div className="header__logo">
       <div className="header__logo_pic">
@@ -38,8 +38,56 @@ function App() {
     <div className="main__nav">
       <span><b className="b-color">Home /</b> Books</span>
     </div>
+    <div className="main__block">
+      <h1>Конструктор бейджиков/аккредитации</h1>
+      <p className="sub_content">Выберите на сколько людей нужно это сделать?</p>
+      <div className="main__block_menu_select">
+        <div className="main__block_menu_select_block green_back">
+          <div className="main__block_menu_select_block_icon green_temp">
+            <img className="main__block_menu_select_block_icon_img" loading="lazy" src={process.env.PUBLIC_URL+"/img/person.svg"} alt="icon"/>
+          </div>
+          <div className="main__block_menu_select_main">
+            <h5>Для одного человека</h5>
+          </div>
+          <div className="main__block_menu_select_main">
+            <p className="main__block_menu_select_main_p">Нужно для одного человека? Не беда, просто используйте наш удобный конструктор и создавайте</p>
+          </div>
+          <div className="main__block_menu_select_main">
+            <button className="main__block_menu_select_main_button green_border">Начать</button>
+          </div>
+        </div>
+        <div className="main__block_menu_select_block purple_back">
+          <div className="main__block_menu_select_block_icon purple_temp">
+            <img className="main__block_menu_select_block_icon_img" loading="lazy" src={process.env.PUBLIC_URL+"/img/group.svg"} alt="icon"/>
+          </div>
+          <div className="main__block_menu_select_main">
+            <h5>Для несколько людей</h5>
+          </div>
+          <div className="main__block_menu_select_main">
+            <p className="main__block_menu_select_main_p">Нужно для несколько людей? Не беда, просто используйте наш удобный конструктор и создавайте</p>
+          </div>
+          <div className="main__block_menu_select_main">
+            <button className="main__block_menu_select_main_button purple_border">Начать</button>
+          </div>
+        </div>
+        <div className="main__block_menu_select_block blue_back">
+          <div className="main__block_menu_select_block_icon blue_temp">
+            <img className="main__block_menu_select_block_icon_img" loading="lazy" src={process.env.PUBLIC_URL+"/img/help.svg"} alt="icon"/>
+          </div>
+          <div className="main__block_menu_select_main">
+            <h5>Инфографика</h5>
+          </div>
+          <div className="main__block_menu_select_main">
+            <p className="main__block_menu_select_main_p">Хотите подробно узнать как можно использовать конструктор?</p>
+          </div>
+          <div className="main__block_menu_select_main">
+            <button className="main__block_menu_select_main_button blue_border">Начать</button>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
-</div>
+</>
 
   );
 }
