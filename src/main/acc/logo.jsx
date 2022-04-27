12c25fa8ca4eead7a4b1a52b69279a687a7d44e5
Dreamer <temirkhan.onyx@gmail.com>
2022-04-27 22:23:28 +0600

    header changed

--- src/main/acc/logo.jsx
+++ src/main/acc/logo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import './acc.css'
 
 const LogoAcc = () => {
     const [logo,setLogo] = useState(process.env.PUBLIC_URL+"/img/logo_round.svg");
--- src/main/index.jsx
+++ src/main/index.jsx
@@ -1,16 +1,17 @@
+import {lazy,Suspense} from 'react';
 import { Routes,Route } from "react-router-dom";
-import Acc from "./acc";
-import LogoAcc from "./acc/logo";
-import SizeAcc from "./acc/size";
+const Acc = lazy(()=>import('./acc'));
+const LogoAcc = lazy(()=>import('./acc/logo'));
+const SizeAcc = lazy(()=>import('./acc/size'));
 
 
 const Main = () => {
     return(
         <main>
             <Routes>
-                <Route path={"/"} element={<Acc/>}/>
-                <Route path={"/logo"} element={<LogoAcc/>}/>
-                <Route path={"/size"} element={<SizeAcc/>}/>
+                <Route path={"/"} element={<Suspense fallback={<div>Загрузка</div>}><Acc/></Suspense>}/>
+                <Route path={"/logo"} element={<Suspense fallback={<div>Загрузка</div>}><LogoAcc/></Suspense>}/>
+                <Route path={"/size"} element={<Suspense fallback={<div>Загрузка</div>}><SizeAcc/></Suspense>}/>
             </Routes>
         </main>
     );
