4f0cef9610133990ab6ea25689987c1190aea1f0
Dreamer <temirkhan.onyx@gmail.com>
2022-05-16 18:46:04 +0600

    sw changed

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,84 +1,41 @@
-// const CACHE_NAME = "version-2";
-// const urlsToCache = ['style.css', 'offline.html'];
+const CACHE_NAME = "version-2";
+const urlsToCache = ['style.css', 'offline.html'];
 
-// const self = this;
-
-// self.addEventListener('install', (event) => {
-//     event.waitUntil(
-//         caches.open(CACHE_NAME)
-//             .then((cache) => {
-//                 console.log('Opened cache');
-
-//                 return cache.addAll(urlsToCache);
-//             })
-//     )
-// });
-
-// self.addEventListener('fetch', (event) => {
-//     event.respondWith(
-//         caches.match(event.request)
-//             .then(() => {
-//                 return fetch(event.request) 
-//                     .catch(() => caches.match('offline.html'))
-//             })
-//     )
-// });
-
-// self.addEventListener('activate', (event) => {
-//     const cacheWhitelist = [];
-//     cacheWhitelist.push(CACHE_NAME);
-
-//     event.waitUntil(
-//         caches.keys().then((cacheNames) => Promise.all(
-//             cacheNames.map((cacheName) => {
-//                 if(!cacheWhitelist.includes(cacheName)) {
-//                     return caches.delete(cacheName);
-//                 }
-//             })
-//         ))
-            
-//     )
-// });
-
-const OFFLINE_VERSION = 1;
-const CACHE_NAME = 'offline';
-const OFFLINE_URL = 'offline.html';
+const self = this;
 
 self.addEventListener('install', (event) => {
-  event.waitUntil((async () => {
-    const cache = await caches.open(CACHE_NAME);
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                console.log('Opened cache');
 
-    await cache.add(new Request(OFFLINE_URL, {cache: 'reload'}));
-  })());
-});
-
-self.addEventListener('activate', (event) => {
-  event.waitUntil((async () => {
-    if ('navigationPreload' in self.registration) {
-      await self.registration.navigationPreload.enable();
-    }
-  })());
-  self.clients.claim();
+                return cache.addAll(urlsToCache);
+            })
+    )
 });
 
 self.addEventListener('fetch', (event) => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith((async () => {
-      try {
-        const preloadResponse = await event.preloadResponse;
-        if (preloadResponse) {
-          return preloadResponse;
-        }
+    event.respondWith(
+        caches.match(event.request)
+            .then(() => {
+                return fetch(event.request) 
+                    .catch(() => caches.match('offline.html'))
+            })
+    )
+});
 
-        const networkResponse = await fetch(event.request);
-        return networkResponse;
-      } catch (error) {
-        console.log('Fetch failed; returning offline page instead.', error);
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [];
+    cacheWhitelist.push(CACHE_NAME);
 
-        const cache = await caches.open(CACHE_NAME);
-        const cachedResponse = await cache.match(OFFLINE_URL);
-        return cachedResponse;
-      }
-    })());
-  }
+    event.waitUntil(
+        caches.keys().then((cacheNames) => Promise.all(
+            cacheNames.map((cacheName) => {
+                if(!cacheWhitelist.includes(cacheName)) {
+                    return caches.delete(cacheName);
+                }
+            })
+        ))
+            
+    )
 });
